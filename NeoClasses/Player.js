2e0e3af751443baad6f683f077fcabdc60804e8a
Dabab001 <dabab001@gold.ac.uk>
2024-12-02 11:24:44 +0000

    Update Player.js

--- NeoClasses/Player.js
+++ NeoClasses/Player.js
@@ -1,54 +1,50 @@
-import Rect from "./Rect.js";
-
-export default class Player {
-  constructor(position, size, color,tilemap) {
-    this.size = size || [8, 16];
-    this.color = color || "Red";
-    this.direction = { up: false, down: false, right: false, left: false };
-    this.tilemap = tilemap
-    this.rect= new Rect(position, this.size, this.color); 
-    this.rect.width+= 2
-    this.rect.height += 2 
-
+export default class Rect {
+  constructor(p5,position,color,size = [16, 16],border = 0,borderRadius = [0, 0, 0, 0]){
+    this.position= p5.createVector(...position)
+    this.velocity= p5.createVector(0,0)
+    this.top = this.position.y
+    this.left = this.position.x;
+    this.bottom = this.top + this.height;
+    this.right = this.left + this.width;
+    [this.width, this.height] = size
+    this.center = this.position.copy()
+    this.center.div(2)
+    [this.centerx, this.centery] = this.center;
+    this.strokeWeight = border
+    this.borderRadii = borderRadius;
+    [this.borderRadii_tl,this.borderRadii_tr, this.borderRadii_br,this.borderRadii_bl] = borderRadius;
+    this.color = color || "blue";
   }
 
+  colliderect(rect) {
+    if (this.right > rect.left && this.left < rect.right && this.bottom > rect.top && this.top < rect.bottom) 
+      {
+      return true;
+    }
+    return false;
+  }
 
   
-  
 
-  update(p5, scroll_offset = [0, 0]) {
-    // Move player using arrow keys
-    this.direction.down=false
-    this.direction.up=false
-    this.direction.right=false
-    this.direction.left=false
-
-    if (p5.keyIsDown(p5.UP_ARROW)) {
-      this.rect.y -= 1;
-      this.direction["up"] = true;
-      this.direction["down"] = false;
-    } else 
-    if (p5.keyIsDown(p5.RIGHT_ARROW)) {
-      this.rect.x += 1;
-      this.direction["right"] = true;
-      this.direction["left"] = false;
-    } 
-    if (p5.keyIsDown(p5.LEFT_ARROW)) {
-      this.rect.x -= 1;
-      this.direction["left"] = true;
-      this.direction["right"] = false;
-    } else
-    if (p5.keyIsDown(p5.DOWN_ARROW)) {
-      this.rect.y += 1;
-      this.direction["down"] = true;
-      this.direction["up"] = false
-    } 
-      
-
-   
     
     
-  
-    this.rect.draw(p5, scroll_offset); // Draw the player's rectangle
+
+
+  update() {
+    this.position.add(this.velocity)
+    this.top = this.position.y
+    this.left = this.position.x;
+    this.bottom = this.top + this.height;
+    this.right = this.left + this.width;
+    this.center = this.position.copy()
+    this.center.div(2)
+    [this.centerx, this.centery] = this.center;
+  }
+
+  draw(p5, scroll_offset = [0, 0]) {
+    this.update()
+    p5.fill(this.color);
+    p5.strokeWeight(this.strokeWeight);
+    p5.rect(this.position.x + scroll_offset[0],this.position.y + scroll_offset[1],this.width,this.height,...this.borderRadii); 
   }
 }
