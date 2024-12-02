cc033ad9db461e9332ac525fe359e7466c024cec
Dabab001 <dabab001@gold.ac.uk>
2024-12-02 11:25:56 +0000

    Update Player.js

--- NeoClasses/Player.js
+++ NeoClasses/Player.js
@@ -1,50 +1,25 @@
-export default class Rect {
-  constructor(p5,position,color,size = [16, 16],border = 0,borderRadius = [0, 0, 0, 0]){
-    this.position= p5.createVector(...position)
-    this.velocity= p5.createVector(0,0)
-    this.top = this.position.y
-    this.left = this.position.x;
-    this.bottom = this.top + this.height;
-    this.right = this.left + this.width;
-    [this.width, this.height] = size
-    this.center = this.position.copy()
-    this.center.div(2)
-    [this.centerx, this.centery] = this.center;
-    this.strokeWeight = border
-    this.borderRadii = borderRadius;
-    [this.borderRadii_tl,this.borderRadii_tr, this.borderRadii_br,this.borderRadii_bl] = borderRadius;
-    this.color = color || "blue";
+import Rect from "./Rect.js";
+
+export default class Player {
+  constructor(p5,tilemap,position,color,size) {
+    this.size = size || [8, 16];
+    this.color = color || "Red";
+    this.rect= new Rect(p5,position,this.color,this.size);
+    this.tilemap = tilemap
+  }  
+
+  update(p5, scroll_offset = [0, 0]) {
+    let vertical = p5.keyIsDown(p5.DOWN_ARROW) - p5.keyIsDown(p5.UP_ARROW)
+    let horizontal = p5.keyIsDown(p5.RIGHT_ARROW) - p5.keyIsDown(p5.LEFT_ARROW)
+    let targetvel  = p5.createVector(horizontal,vertical)
+    this.rect.velocity = targetvel
+    this.collision(p5)
+    this.rect.draw(p5, scroll_offset); // Draw the player's rectangle
   }
 
-  colliderect(rect) {
-    if (this.right > rect.left && this.left < rect.right && this.bottom > rect.top && this.top < rect.bottom) 
-      {
-      return true;
-    }
-    return false;
-  }
+  collision(p5){
+    let rects = this.tilemap.give_colision(this.rect.position.array(),p5)
+    console.log(rects)
 
-  
-
-    
-    
-
-
-  update() {
-    this.position.add(this.velocity)
-    this.top = this.position.y
-    this.left = this.position.x;
-    this.bottom = this.top + this.height;
-    this.right = this.left + this.width;
-    this.center = this.position.copy()
-    this.center.div(2)
-    [this.centerx, this.centery] = this.center;
-  }
-
-  draw(p5, scroll_offset = [0, 0]) {
-    this.update()
-    p5.fill(this.color);
-    p5.strokeWeight(this.strokeWeight);
-    p5.rect(this.position.x + scroll_offset[0],this.position.y + scroll_offset[1],this.width,this.height,...this.borderRadii); 
   }
 }
