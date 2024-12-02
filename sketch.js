45dd6daac935142efae603a317e8dafff078c6c6
Dabab001 <dabab001@gold.ac.uk>
2024-12-02 11:27:14 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,20 +1,19 @@
-// import file from "./levelmap.json" with { type: "json" };
-// import Tilemap from "./NeoClasses/tilemap.js";
-import Rect from "./NeoClasses/Rect.js";
-let rect1;
+import file from "./levelmap.json" with { type: "json" };
+import Tilemap from "./NeoClasses/tilemap.js";
 
-// let game = new Tilemap();
+let game;
+
+
+
 new p5(function(p5){
 p5.setup = function() {
   p5.createCanvas(640, 480);
-  rect1= new Rect([0,0],p5)
-
-  // game.Setup(file,p5);
+  game = new Tilemap(p5)
+  game.Setup(file,p5);
 
 }
 
  p5.draw = function() {
   p5.background("gray");
-  rect1.draw(p5)
-  // game.Render(p5)
+  game.Render(p5)
 }})
